f234605d971e4f600090acc127dc170dba160862
christian <christian.ambriz@gmail.com>
2016-12-03 16:07:36 -0800

    adds ability to logout and login

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { logout } from '../actions/authActions';
 
 class NavigationBar extends Component {
+  constructor(props) {
+    super(props)
+    this.logout. = this.logout..bind(this);
+  }
+
+  logout(event) {
+    event.preventDefault();
+    this.props.logout();
+  }
+
   render () {
+    const { isAuthenticated } = this.props.auth;
+
+    const userLinks = (
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#" onClick={this.logout}>Logout</a></li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="nav navbar-nav navbar-right">
+        <li><Link to="/signup">Sign up</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    );
+
     return (
       <nav className="navbar navbar-default">
         <div className="container-fluid">
@@ -11,10 +38,7 @@ class NavigationBar extends Component {
           </div>
 
           <div className="collapse navbar-collapse">
-            <ul className="nav navbar-nav navbar-right">
-              <li><Link to="/signup">Sign up</Link></li>
-              <li><Link to="/login">Login</Link></li>
-            </ul>
+            { isAuthenticated ? userLinks : guestLinks }
           </div>
         </div>
       </nav>
@@ -22,4 +46,15 @@ class NavigationBar extends Component {
   }
 }
 
-export default NavigationBar;
+NavigationBar.propTypes = {
+  auth: React.PropTypes.object.isRequired,
+  logout: React.PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
